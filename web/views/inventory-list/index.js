8bff3bc3231631ce611afbf9536ab13eac99cccb
Sneha Jain <Sneha.T.Jain@in.tesco.com>
2016-12-11 09:04:58 +0530

    product crud over

--- web/views/inventory-list/index.js
+++ web/views/inventory-list/index.js
@@ -19,15 +19,22 @@ var Product = {
       evt.preventDefault();
       var target = $(evt.target);
       $('#ajaxLoader').show();
-      $.post(url + '/inventory/delete/' + target.attr('data-id'), function() {
+      $.post(url + '/product/delete/' + target.attr('data-id'), function() {
         $('#ajaxLoader').hide();
         self.getList();
       });
     });
     $('#productList').on('click', '.edit', function(evt) {
       evt.preventDefault();
+      var target = $(evt.target);
+      var row = target.parents('tr');
       $('#productModal').show();
       $('#productModal').attr('type', 'editProduct');
+      $('#productModal input').val(row.find('td:eq(1)').text());
+      $('#productModal select:eq(0)').val(row.find('td:eq(2)').text().toLowerCase());
+      $('#productModal select:eq(1)').val(row.find('td:eq(3)').text().toLowerCase());
+      $('#productModal textarea').val(row.find('td:eq(4)').text());
+      $('#productModal').attr('data-id',row.find('td:eq(0)').text());
       $('#productModal .buttongroup button[type=submit]').text('Update');
     });
     $('.buttongroup button').on('click', function(evt) {
@@ -37,14 +44,14 @@ var Product = {
         self.closeModal();
       } else if (target.parents('#productModal').attr('type') === 'addProduct') {
         $('#ajaxLoader').show();
-        $.post(url + '/inventory/create/?' + target.parents('form').serialize(), function() {
+        $.post(url + '/product/create/?' + target.parents('form').serialize(), function() {
           $('#ajaxLoader').hide();
           self.closeModal();
           self.getList();
         });
       } else if (target.parents('#productModal').attr('type') === 'editProduct') {
         $('#ajaxLoader').show();
-        $.post(url + '/inventory/update/?' + target.parents('form').serialize(), function() {
+        $.post(url + '/product/update/' + target.parents('#productModal').attr('data-id'), target.parents('form').serialize() ,function() {
           $('#ajaxLoader').hide();
           self.closeModal();
           self.getList();
@@ -54,14 +61,16 @@ var Product = {
   },
   getList: function() {
     $('#ajaxLoader').show();
-    $.get(url + '/inventory', function(data) {
+    $.get(url + '/product', function(data) {
       $('#ajaxLoader').hide();
       $('#productList tr:nth-child(n+2)').remove();
       data.forEach(function(item) {
         var row = '';
-        Object.keys(item).forEach(function(key) {
-          row += '<td>' + item[key] + '</td>';
-        });
+        row += '<td>' + item.item_id + '</td>';
+        row += '<td>' + item.name + '</td>';
+        row += '<td>' + item.quality + '</td>';
+        row += '<td>' + item.unit + '</td>';
+        row += '<td>' + item.description + '</td>';
         row += '<td><button class="edit">Edit</button><button class="delete" data-id="' + item.item_id + '">Delete</button></td>';
         $('#productList').append('<tr>' + row + '</tr>');
       });
@@ -70,5 +79,9 @@ var Product = {
   closeModal: function() {
     $('#productModal').hide();
     $('#productModal').removeAttr('type');
+    $('#productModal').removeAttr('data-id');
+    $('#productModal form input').val('');
+    $('#productModal form select').val('');
+    $('#productModal form textarea').val('');
   }
 };
