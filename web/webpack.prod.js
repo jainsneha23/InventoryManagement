dbbea018614fba80acb91f0cd3de4268e7d56436
Sneha Jain <mystery.sneha@gmail.com>
2016-12-19 23:43:26 +0530

    .

--- web/webpack.prod.js
+++ web/webpack.prod.js
@@ -18,7 +18,21 @@ export default {
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
+    stats: {
+      colors: true,
+      hash: false,
+      version: false,
+      timings: false,
+      assets: false,
+      chunks: false,
+      modules: false,
+      reasons: false,
+      children: false,
+      errors: true,
+      errorDetails: true,
+      warnings: false
+    }
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
