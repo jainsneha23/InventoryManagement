1620b8103cb3d44cf56c173a65e70a08eb11ee85
Sneha Jain <mystery.sneha@gmail.com>
2016-12-21 11:01:04 +0530

    adding routes

--- web/entry.js
+++ web/entry.js
@@ -6,6 +6,8 @@ import App from './pages/app';
 import Home from './pages/home';
 import Product from './pages/product';
 import Customer from './pages/customer';
+import Dealer from './pages/dealer';
+import Stock from './pages/stock';
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
@@ -16,6 +18,8 @@ render((
          <IndexRoute component = {Home} />
          <Route path = "product" component = {Product} />
          <Route path = "customer" component = {Customer} />
+         <Route path = "dealer" component = {Dealer} />
+         <Route path = "stock" component = {Stock} />
       </Route>
    </Router>
 ), document.getElementById('app'));
